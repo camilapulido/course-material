d91263b273358fc7da1990b2f4461bbae2c1012e
camilapulido <camilapulido@gmail.com>
2014-09-22 18:43:31 +0200

    solut 007

--- exercices/007/solution.py
+++ exercices/007/solution.py
@@ -4,7 +4,7 @@
 
 Ceci est un script temporaire.
 """
-a = ['H', 'e', 'l', 'l', 'o','','W', 'o', 'r', 'l', 'd']
+a = ['H', 'e', 'l', 'l', 'o','','W', 'o', 'r', 'l', 'd', '!']
 for i in range(len(a)):
      print(a[i])
     
