7511562b1cef2a66057c40fe7843846a06bc7dda
camilapulido <camilapulido@gmail.com>
2014-09-22 18:36:29 +0200

    solut 007

---
+++ exercices/007/solution.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+a = ['H', 'e', 'l', 'l', 'o','','W', 'o', 'r', 'l', 'd']
+for i in range(len(a)):
+     print(a[i])
+    
