edec30670b76673b6a20c18346df8e530b12f9f9
camilapulido <camilapulido@gmail.com>
2014-09-23 18:25:32 +0200

    solut 050

---
+++ exercices/050/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 14:53:08 2014
+
+@author: Camila
+"""
+a = 0
+b = 0
+for i in range(0,1000,3):
+    a = a + i;
+for i in range (5,1000,5):
+    b = b + i;
+print (a+b)
