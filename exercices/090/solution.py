2c3540465e11e4f013439e5ff1dfd4d0d02ba94c
camilapulido <camilapulido@gmail.com>
2014-09-23 18:41:10 +0200

    solut 090

---
+++ exercices/090/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 15:37:06 2014
+
+@author: Camila
+"""
+
+import sys
+i = 0
+#a = list(enumerate(sys.argv))
+for i in range(len(sys.argv)):
+    print (i, (sys.argv[i]))
