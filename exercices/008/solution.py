c493b41b2a0d1d3204067305786ccf3314f47450
camilapulido <camilapulido@gmail.com>
2014-09-23 11:22:38 +0200

    solut 008

--- exercices/008/solution.py
+++ exercices/008/solution.py
@@ -14,4 +14,5 @@ phantom_menace = """Turmoil has engulfed the Galactic Republic. The\
  Chancellor has secretly dispatched two Jedi Knights, the guardians of\
  peace and justice in the galaxy, to settle the conflict"""
 
-print(len(phantom_menace))
+a = phantom_menace.split()
+print(len(a))
