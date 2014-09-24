2a9e525737a1492d88cd339c85d7c640832b1394
camilapulido <camilapulido@gmail.com>
2014-09-24 18:29:02 +0200

    solut 210

--- exercices/200/solution.py
+++ exercices/200/solution.py
@@ -4,10 +4,10 @@ Created on Wed Sep 24 18:07:51 2014
 
 @author: Camila
 """
-from is_prime import *
+import is_prime
 d = 0
 for i in range(1000):
-    a = is_prime(i)
+    a = is_prime.is_prime(i)
     if a ==True:
         d+=1
 print (d)
