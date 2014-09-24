70c47fdc5cc65ef8f8e1698d5ee94a1ea0f318c0
camilapulido <camilapulido@gmail.com>
2014-09-24 18:04:19 +0200

    solut 203

---
+++ exercices/203/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 24 17:37:52 2014
+
+@author: Camila
+"""
+
+def is_multiple(a,b):
+    if a%b == 0:
+        return True
+    else:
+        return False
+    
