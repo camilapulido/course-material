bc855c115e12b0c4a7e83c8ee9909e57f9078df3
camilapulido <camilapulido@gmail.com>
2014-09-24 19:06:49 +0200

    solut 220

---
+++ exercices/220/solution.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 24 18:37:45 2014
+
+@author: Camila
+"""
+
+import is_prime
+import string
+d = 0
+prime= []
+for i in range(10000, 10050):
+    a = is_prime.is_prime(i)
+    if a ==True:
+        d+=1
+        prime =  prime + [i]
+primelst = map(str, prime)
+primelst = ", ".join(primelst)
+print(primelst)
+           
