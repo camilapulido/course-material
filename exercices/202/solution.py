78bcdaa62161fbddb2a6f012966ca89e8e81e217
camilapulido <camilapulido@gmail.com>
2014-09-24 17:37:03 +0200

    solut 202

---
+++ exercices/202/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 24 17:31:26 2014
+
+@author: Camila
+"""
+
+def start_with(word,b):  
+    if word[0] == b:               
+        return True
+    else:
+        return False
